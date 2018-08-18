d1376f6f93927009bec0bf4a20ea6b815a707330
YWJamesLin <ywjameslin@gmail.com>
2018-08-18 15:56:10 +0800

    webview: fix on webview.js
    fix bug: cannot view images from imgur due to forbiden from domain
    ptt.cc

---
+++ webview.js
@@ -0,0 +1,6 @@
+module.exports = (Franz) => {
+  let meta = document.createElement('meta');
+  meta.name = "referrer";
+  meta.content = "no-referrer";
+  document.getElementsByTagName('head')[0].appendChild(meta);
+}
